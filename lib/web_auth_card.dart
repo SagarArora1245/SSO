6a644ac3682ea4c3534e007971ddb34215eae71a
sagar12345 <sagar@softserv.in>
2023-01-23 11:34:28 +0530

    implemented sso

---
+++ lib/web_auth_card.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+class WebAuthCard extends StatelessWidget {
+  final String label;
+  final Future<void> Function() action;
+
+  const WebAuthCard(
+  {required final this.label, required final this.action, final Key? key})
+: super(key: key);
+
+@override
+Widget build(final BuildContext context) {
+  return Card(
+      child: Padding(
+          padding: const EdgeInsets.all(padding),
+          child: SizedBox(
+              width: double.maxFinite,
+              child: Semantics(
+                label: label,
+                child: ElevatedButton(
+                  key: Key(label),
+                  onPressed: action,
+                  child: Text(label),
+                ),
+              ))));
+}
+}
